892becf8d0f78824ff36a90773f9c32084bb7be6
ivanovmain <49336868+ivanovmain@users.noreply.github.com>
2020-02-01 21:49:19 +0700

    add react app test task

---
+++ iv-react-app/precache-manifest.353dc52d6119d0e683bab1dcb5692271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa23622e99b96dc2132d7b9b2b1bc75a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "540d40dca2802cd54d62",
+    "url": "/static/css/main.60530cb2.chunk.css"
+  },
+  {
+    "revision": "13bc1c268f9e10b02edf",
+    "url": "/static/js/2.b5220e4a.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/static/js/2.b5220e4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "540d40dca2802cd54d62",
+    "url": "/static/js/main.473ee278.chunk.js"
+  },
+  {
+    "revision": "070db8a3c8ae3e511328",
+    "url": "/static/js/runtime-main.2546e0bf.js"
+  }
+]);
